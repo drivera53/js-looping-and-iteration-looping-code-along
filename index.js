6452b7404e729a673d52533030ecbb6fd3893a3f
Danny Rivera <samboard@hotmail.com>
2021-05-03 16:31:38 -0400

    commit tree 7b4346bf3e6fc369490fa6a89e996984e3083757

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+// Returns an array of thank you messages for each name provided to the function
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+}
+
+function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+}
